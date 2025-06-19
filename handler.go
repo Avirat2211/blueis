5ea73a557ec383923553d303d3ca4df79efc88ad
Avirat Joshi <joshi.avirat2211@gmail.com>
2025-06-19 19:42:19 +0530

    Added HGETALL

--- handler.go
+++ handler.go
@@ -1,14 +1,16 @@
 package main
 
-import "sync"
-
-var SETs = map[string]string{}
-var SETsMutex = sync.RWMutex{}
+import (
+	"sync"
+)
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetAll,
 }
 
 func ping(args []Value) Value {
@@ -17,6 +19,9 @@ func ping(args []Value) Value {
 	}
 	return Value{typ: "string", str: args[0].bulk}
 }
+
+var SETs = map[string]string{}
+var SETsMutex = sync.RWMutex{}
 
 func set(args []Value) Value {
 	if len(args) != 2 {
@@ -35,7 +40,7 @@ func set(args []Value) Value {
 
 func get(args []Value) Value {
 	if len(args) != 1 {
-		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+		return Value{typ: "error", str: "Wrong number of arguments for GET command"}
 	}
 
 	key := args[0].bulk
@@ -49,4 +54,59 @@ func get(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: value}
+}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMutex = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "Wrong number of arguments for HSET command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMutex.Lock()
+	_, err := HSETs[hash]
+	if !err {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMutex.Unlock()
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "Wrong number of arguments for HGET command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	HSETsMutex.Lock()
+	value, err := HSETs[hash][key]
+	HSETsMutex.Unlock()
+	if !err {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hgetAll(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "Wrong number of arguments for HGETALL command"}
+	}
+	hash := args[0].bulk
+	HSETsMutex.Lock()
+	val, ok := HSETs[hash]
+	if !ok {
+		return Value{typ: "array", array: []Value{}}
+	}
+	HSETsMutex.Unlock()
+	var value []Value
+	for x, y := range val {
+		value = append(value, Value{typ: "bulk", bulk: x})
+		value = append(value, Value{typ: "bulk", bulk: y})
+	}
+	return Value{typ: "array", array: value}
 }
